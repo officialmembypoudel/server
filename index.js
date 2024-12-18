84daaa783b33de38b2552510eb57c21f589e908e
Memby Poudel <officialmembypoudel@gmail.com>
2024-12-18 15:35:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+import http from 'http';
+import { WebSocket, WebSocketServer } from 'ws';
+import { v4 as uuidGenerator } from 'uuid';
+
+import { states } from './model/state.js';
+import { client } from './model/user.js';
+
+const PORT = 443;
+
+const server = http.createServer();
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`Received message => ${message}`);
+    try {
+      const data = JSON.parse(message);
+
+      if (data.messasge === 'fetch') {
+        ws.send(
+          JSON.stringify({
+            success: true,
+            message: 'fetch',
+            data: states,
+            source: 'server',
+          })
+        );
+      } else if (data.message === 'update') {
+        // find device
+        let gadget = states.filter((state) => state.name === data.name);
+        if (gadget.length > 0) {
+          gadget = gadget[0];
+
+          if (gadget.name === 'led') {
+            gadget.state = data.state;
+
+            wss.clients.forEach((client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(
+                  JSON.stringify({
+                    success: true,
+                    message: 'update',
+                    data: gadget,
+                    source: 'server',
+                  })
+                );
+              }
+            });
+          } else if (gadget.name === 'potentiometer') {
+            gadget.value = data.value;
+            ws.send(
+              JSON.stringify({
+                success: true,
+                message: 'update',
+                data: gadget,
+                source: 'server',
+              })
+            );
+          } else {
+            ws.send(
+              JSON.stringify({
+                success: false,
+                message: 'update',
+                data: 'State not found',
+                source: 'server',
+              })
+            );
+          }
+        } else {
+          ws.send(
+            JSON.stringify({
+              success: false,
+              message: 'Unknown message',
+              data: null,
+            })
+          );
+        }
+      } else {
+        ws.send(
+          JSON.stringify({
+            success: false,
+            message: 'Unknown message',
+            data: null,
+          })
+        );
+      }
+    } catch (error) {
+      ws.send('Error parsing JSON' + error?.message);
+    }
+
+    ws.send(`Hello, you sent => ${message}`);
+  });
+  ws.send('Hi there, I am a WebSocket server');
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Listening on http://localhost:' + PORT);
+});
---
+++ model/state.js
@@ -0,0 +1,12 @@
+export const states = [
+  {
+    id: 1,
+    name: 'led',
+    state: false,
+  },
+  {
+    id: 2,
+    name: 'potentiometer',
+    value: 0,
+  },
+];
